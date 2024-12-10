7ad27c3253d04246a259018f217656f72fe2dd33
Shoufang Wu <Shoufang.W@arbin.com>
2024-12-10 16:39:19 -0600

    rm datamodel

--- ctitoolbox/__init__.py
+++ ctitoolbox/__init__.py
@@ -5,4 +5,16 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 clr.AddReference("System")
 clr.AddReference(os.path.join(current_dir, "bin", "ArbinCTI"))
-clr.AddReference(os.path.join(current_dir, "bin", "ArbinDataModel"))
+
+from ctitoolbox.src.cs_type_converter import CSTypeConverter
+from ctitoolbox.src.cs_type_converter import (
+    TE_DATA_TYPE, 
+    Enum_MvUd,
+    EReadWriteMode,
+    StartResumeEx,
+    MetaVariableInfo,
+    MetaVariableInfoEx,
+    TimeSensitiveSetMV,
+    TimeSensitiveSetMVArgs,
+    CMetavariableDataCodeApply
+)
