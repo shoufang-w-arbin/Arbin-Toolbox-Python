0e2d540472d52e19d24fd334a9375e3002b81426
Stefan Wu <sfwu@tamu.edu>
2025-06-03 09:39:40 +0800

    bug fix: QA6685, QA6694, QA6699, QA6765, QA6765, QA6786, QA6776

--- arbinclienttools/src/feedback/ttest_management.py
+++ arbinclienttools/src/feedback/ttest_management.py
@@ -55,7 +55,7 @@ class AssignFileFeedback(DictReprBase):
         def __init__(self, obj):
             self.channel_id     = int(obj.ChannelID)
             self.result         = str(obj.Result)
-            self.assign_result  = EAssignFileResult(int(obj.AssignFileResult))
+            self.assign_result  = EAssignFileResult(int(obj.AssignResult))
 
     def __init__(self, obj):
         if not isinstance(obj, ArbinDataModel.TestManagement.AssignFileFDBK):
@@ -65,10 +65,10 @@ class AssignFileFeedback(DictReprBase):
         self.is_success             = bool(obj.IsSuccess)
         self.sn                     = int(obj.SN)
         
-class UpdateMetaVariableFeedback(DictReprBase):
+class UpdateMetaVariablesFeedback(DictReprBase):
     def __init__(self, obj):
-        if not isinstance(obj, ArbinDataModel.TestManagement.UpdateMetaVariableFDBK):
-            raise ValueError("'obj' must be of type ArbinDataModel.TestManagement.UpdateMetaVariableFDBK")
+        if not isinstance(obj, ArbinDataModel.TestManagement.UpdateMetaVariablesFDBK):
+            raise ValueError("'obj' must be of type ArbinDataModel.TestManagement.UpdateMetaVariablesFDBK")
         self.meta_variable_info = [AIMetaVariableInfo(x) for x in obj.MetaVariableInfos]
         self.sn                 = int(obj.SN)
 
